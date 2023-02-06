6a4f4601f0ed20d7cb92b81a5da7104b47ee853d
shamspias <shamsuddinpias0@gmail.com>
2023-02-07 01:45:17 +0600

    add stripe in setting

--- config/settings/common.py
+++ config/settings/common.py
@@ -41,6 +41,7 @@ LOCAL_APPS = [
     'ausers.apps.AusersConfig',
     'common.apps.CommonConfig',
     'notifications.apps.NotificationsConfig',
+    'stripe_payment.apps.StripePaymentConfig',
 
 ]
 
