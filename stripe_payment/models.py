a1feb0a4e5d4914832e1f138fe36563854ad0792
shamspias <shamsuddinpias0@gmail.com>
2023-02-07 01:44:33 +0600

    fix: subscription model

--- stripe_payment/models.py
+++ stripe_payment/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from ausers.models import User
+
+
+class Subscription(models.Model):
+    """
+    Model to Maintain subscription
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscription_plan = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    active = models.BooleanField(default=True)
+    payment_status = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        # Check if the subscription has ended
+        if self.end_date < timezone.now().date() and self.payment_status:
+            # Set payment status to False
+            self.payment_status = False
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.subscription_plan
