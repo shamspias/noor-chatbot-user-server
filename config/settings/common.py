4db7d10a7c997902c227de32a5bcb417a80e97d2
shamspias <shamsuddinpias0@gmail.com>
2023-02-07 16:00:42 +0600

    fix: csrf

--- config/settings/common.py
+++ config/settings/common.py
@@ -145,10 +145,10 @@ CORS_ALLOW_METHODS = (
     'OPTIONS'
 )
 
-# CORS_ALLOW_HEADERS = list(default_headers) + [
-#     'X-CSRFToken',
-# ]
-# CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'X-CSRFToken',
+]
+CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
 
 # GENERALS
 APPEND_SLASH = bool(os.getenv('APPEND_SLASH', True))
