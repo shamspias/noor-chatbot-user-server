bec3dc33f434d26d6158825476c9a4f70a1ffe86
shamspias <shamsuddinpias0@gmail.com>
2023-02-19 03:26:20 +0600

    fix: request

--- stripe_payment/views.py
+++ stripe_payment/views.py
@@ -21,83 +21,85 @@ def create_user_from_stripe(obj):
 
 @csrf_exempt
 def stripe_webhook(request):
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
-    event = None
+    if request.method == 'POST':
+        payload = request.body
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+        endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+        event = None
 
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        except ValueError as e:
+            # Invalid payload
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return HttpResponse(status=400)
 
-    # Handle the event
-    if event['type'] == 'checkout.session.async_payment_succeeded':
-        session = event['data']['object']
+        # Handle the event
+        if event['type'] == 'checkout.session.async_payment_succeeded':
+            session = event['data']['object']
 
-    elif event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-    elif event['type'] == 'customer.created':
-        customer = event['data']['object']
-        # Get the email address
-        email = customer['billing_details']['email']
-        phone = customer['billing_details']['phone']
-        name = customer['billing_details']['name']
-        stripe_id = customer['billing_details']['id']
-        # Get the user from the database
-        if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email)
-            # Update the payment status of the user
-            user.phone_number = phone
-            user.first_name = name
-            user.stripe_id = stripe_id
-            user.save()
+        elif event['type'] == 'checkout.session.completed':
+            session = event['data']['object']
+
+        elif event['type'] == 'customer.created' or event['type'] == 'customer.updated':
+            customer = event['data']['object']
+            # Get the email address
+            email = customer['billing_details']['email']
+            phone = customer['billing_details']['phone']
+            name = customer['billing_details']['name']
+            stripe_id = customer['billing_details']['id']
+            # Get the user from the database
+            if User.objects.filter(email=email).exists():
+                user = User.objects.get(email=email)
+                # Update the payment status of the user
+                user.phone_number = phone
+                user.first_name = name
+                user.stripe_id = stripe_id
+                user.save()
+            else:
+                create_user_from_stripe(customer)
+
+        elif event['type'] == 'customer.deleted':
+            customer = event['data']['object']
+            email = customer['billing_details']['email']
+            if User.objects.filter(email=email).exists():
+                user = User.objects.get(email=email)
+                user.delete()
+
+        elif event['type'] == 'customer.subscription.created' or event['type'] == 'customer.subscription.resumed':
+            subscription = event['data']['object']
+            email = subscription['billing_details']['email']
+            if User.objects.filter(email=email).exists():
+                user = User.objects.get(email=email)
+                # Update the payment status of the user
+                user.subscription_status = True
+                user.save()
+            else:
+                user = create_user_from_stripe(subscription)
+                user.subscription_status = True
+                user.save()
+
+        elif event['type'] == 'customer.subscription.deleted' or event['type'] == 'customer.subscription.paused':
+            subscription = event['data']['object']
+            email = subscription['billing_details']['email']
+            if User.objects.filter(email=email).exists():
+                user = User.objects.get(email=email)
+                # Update the payment status of the user
+                user.subscription_status = False
+                user.save()
+            else:
+                user = create_user_from_stripe(subscription)
+                user.subscription_status = False
+                user.save()
+
+        elif event['type'] == 'customer.subscription.trial_will_end':
+            subscription = event['data']['object']
+        # ... handle other event types
         else:
-            create_user_from_stripe(customer)
+            print('Unhandled event type {}'.format(event['type']))
 
-    elif event['type'] == 'customer.deleted':
-        customer = event['data']['object']
-        email = customer['billing_details']['email']
-        if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email)
-            user.delete()
-
-    elif event['type'] == 'customer.subscription.created' or event['type'] == 'customer.subscription.resumed':
-        subscription = event['data']['object']
-        email = subscription['billing_details']['email']
-        if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email)
-            # Update the payment status of the user
-            user.subscription_status = True
-            user.save()
-        else:
-            user = create_user_from_stripe(subscription)
-            user.subscription_status = True
-            user.save()
-
-    elif event['type'] == 'customer.subscription.deleted' or event['type'] == 'customer.subscription.paused':
-        subscription = event['data']['object']
-        email = subscription['billing_details']['email']
-        if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email)
-            # Update the payment status of the user
-            user.subscription_status = False
-            user.save()
-        else:
-            user = create_user_from_stripe(subscription)
-            user.subscription_status = False
-            user.save()
-
-    elif event['type'] == 'customer.subscription.trial_will_end':
-        subscription = event['data']['object']
-    # ... handle other event types
-    else:
-        print('Unhandled event type {}'.format(event['type']))
-
-    return HttpResponse(status=200)
+        return HttpResponse(status=200)
