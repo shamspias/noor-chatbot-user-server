cbe304799a6bcddeccf6972688dcbbb11f6a5524
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 01:49:08 +0600

    create model

--- prayer_m/admin.py
+++ prayer_m/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ReligiousFiles, PrayerList
 
-# Register your models here.
+admin.site.register(ReligiousFiles)
+admin.site.register(PrayerList)
--- prayer_m/models.py
+++ prayer_m/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class PrayerList(models.Model):
+    """
+    List of prayers
+    """
+    pass
+
+
+class ReligiousFiles(models.Model):
+    """
+    Media or other file related to religion
+    """
+    title = models.CharField(max_length=250, blank=True, null=True)
+    document = models.FileField(upload_to="/media", blank=True, null=True)
+    text_contains = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Islamic Contain"
+        verbose_name_plural = "Islamic Contains"
