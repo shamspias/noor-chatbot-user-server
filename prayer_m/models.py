6376706be93ea8ab7052a901bf28a4f583c61d61
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 23:30:52 +0600

    fix: path and env

--- prayer_m/models.py
+++ prayer_m/models.py
@@ -22,7 +22,7 @@ class ReligiousFiles(models.Model):
     Media or other file related to religion
     """
     title = models.CharField(max_length=250, blank=True, null=True)
-    document = models.FileField(upload_to="/media", blank=True, null=True)
+    document = models.FileField(upload_to="media/", blank=True, null=True)
     text_contains = models.TextField(blank=True, null=True)
     type = models.CharField(max_length=100, blank=True, null=True)
 
